a106bc7a86f9afb0c6da8d8f35b7fff5b87ee5ce
grahamguthrie99 <grahamguthrie99@gmail.com>
2021-11-23 19:49:07 -0500

    completed buildspace tutorial

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xc5c60980E4A47Bd49F318Bb0F695B5e35d5BC106';
+
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+  
+  export { CONTRACT_ADDRESS, transformCharacterData };
